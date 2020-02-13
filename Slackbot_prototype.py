6559d4c97825c3823311a31b4705c54b844c6db5
Michael McGarty <Mcgartym01@yahoo.com>
2020-02-13 13:13:27 -0500

    Minor updates

--- Slackbot_prototype.py
+++ Slackbot_prototype.py
@@ -1,51 +1,97 @@
 from slack import RTMClient
 from time import sleep
 
+# Driver methods for each command.
+# Proper usage instructions
+def helpMe(client, c_e, txt, t_ts):
+    print(client, c_e, txt, t_ts)
+    client.chat_postMessage(
+    channel=c_e,
+    text=txt,
+    thread_ts=t_ts
+    )
+    return
+
+# Time duration of the print.
+def status(w_c, c_e, th):
+    w_c.chat_postMessage(
+    channel=c_e,
+    text="Work In Progress" ,
+    thread_ts=th
+    )
+    return
+
+# File I/O for what they want to print.
+def upload():
+    return
+
+# Sent by user after current print has finished -- updates queue
+def finished():
+    return
+
+# List of people currently waiting to print.
+def queue():
+    return
+
+# Returns snapshot of current print progress
+def snapshot():
+    return
+
+# Begins livestream of current print progress
+def stream():
+    return
+
 @RTMClient.run_on(event="message")
 def print_message(**payload):
-
+    print("Message reveived:")
     data = payload['data']
     web_client = payload['web_client']
 
     try:
         # Debug information
-        print(data['user'])
-        print(data['text'])
-        print(data['channel'])
+        # print(data)
+
+        # print(data['user'])
+        # print(data['text'])
+        # print(data['channel'])
 
         command = data['text'].split(" ")[0]
 
         channel_event = data['channel']
         received_message = data['text']
-        thread = data['ts'] # Lets the bot reply to the message.
+        thread = data['ts'] # Lets the bot reply to the message as a thread
 
-        # Help / Ussage command.
+        # Help / Usage command.
         if command == '!help':
             web_client.chat_postMessage(
-                channel=channel_event,
-                text="Possible Commands: ... WIP",
-                thread_ts=thread
+            channel=channel_event,
+            text="Here's what I can do...",
+            thread_ts=thread
             )
+            # prompt = "WIP: Proper usage:"
+            # helpMe(client, channel_event, prompt, thread)
 
         # Status / number of jobs queued.
         if command == '!status':
-            web_client.chat_postMessage(
-                channel=channel_event,
-                text="WIP: There are currently __ of jobs in queue.",
-                thread_ts=thread
-            ) 
-            
-        # TO DO: 
-        # Status of the current job. 
-        # Completed? Registers that the most recent job was finished
-        # Backend : alert the user when its turn to print.
-        # Upload command.
-        # Snapshot. Sends a picture of the current print as a reply 
-        # Stream? Pulls up live stream of the print as a reply.
+            status(web_client, channel_event, thread)
+
+        # Please don't ddos the printer with uploads.
+        if command == '!upload':
+            print("File Upload:")
+            try:
+                # file_info[0] accesses the first and assumed only file per upload.
+                file_info = data['files']
+                if (file_info[0]['filetype'] != 'java'):
+                    # Error message saying this ain't g. code.
+                    return
+            except:
+                return
+
+            print("Uploaded file {}".format(file_info[0]['filetype']))
 
         return
     except:
         return
 
-rtm_client = RTMClient(token='ELI2_SLACK_TOKEN')
+rtm_client = RTMClient(token='INSERT_TOKEN')
 rtm_client.start()
